0d008df452723f5a4711080667d0920d2a7f266f
Dongyeop Lee <dlduq29@gmail.com>
2025-04-09 03:47:17 +0900

    로그인 페이지 추가 수정

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -45,11 +45,11 @@ const login = () => {
     <div className="w-full min-h-screen flex flex-col justify-center items-center bg-[#f8f9fa] font-[Arial,sans-serif]">
       <img src={Logo} alt="logo" width={200} height={200} />
       <form
-        className="w-[360px] p-5 bg-white border border-gray-300 rounded-[10px] shadow-[0_5px_6px_rgba(0,0,0,0.1)]"
+        className="w-[370px] p-6 bg-white border border-gray-300 rounded-[10px] shadow-lg"
         onSubmit={handleLogin}
       >
         <input
-          className="w-full p-3 my-[10px] border border-gray-300 rounded-[7px] text-[13px] focus:outline-none focus:placeholder:text-[12px] focus:placeholder:font-[Arial,sans-serif]"
+          className="w-full p-3 my-[8px] border border-gray-300 rounded-[7px] text-[13px] focus:border-black focus:text-[12px]"
           type="text"
           id="userid"
           value={userId}
@@ -57,7 +57,7 @@ const login = () => {
           placeholder="아이디"
         />
         <input
-          className="w-full p-3 mb-[10px] border border-gray-300 rounded-[7px] text-[13px] focus:outline-none focus:placeholder:text-[12px] focus:placeholder:font-[Arial,sans-serif]"
+          className="w-full p-3 my-[8px] border border-gray-300 rounded-[7px] text-[13px] focus:border-black focus:text-[12px]"
           type="password"
           id="password"
           value={password}
@@ -66,10 +66,14 @@ const login = () => {
         />
         <button
           type="submit"
-          className="w-full py-2 mt-2 bg-[#bdfcc9] text-white text-[16px] font-bold rounded cursor-pointer"
+          className="w-full py-2 mt-2 bg-[#bdfcc9] text-white text-[17px] font-bold rounded cursor-pointer"
         >
           로그인
         </button>
+        <div className="flex justify-between text-[10px] px-15 pt-2">
+          <a className="cursor-pointer">회원가입</a>
+          <a className="cursor-pointer">비밀번호 찾기</a>
+        </div>
       </form>
     </div>
   );
